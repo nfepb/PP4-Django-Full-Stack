bf5e5ae7ba493ae9c5a586e7e419ba1873a95228
nfepb <nicolas.planteb@gmail.com>
2022-11-17 09:44:40 +0000

    Adds templates base/index/movie_detail.html

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Movie, WatchlistItem
+from .models import Movie, Review, WatchlistItem, Genre
 
-# Create your views here.
+class MovieList(generic.ListView):
+    model = Movie
+    queryset = Movie.objects.order_by('-movie_created_on')
+    template_name = 'index.html'
+    # limiting number of posts that can appear on front page:
+    paginate_by = 8
